f04780a9cc8f7a68d4780c027c552b3a2ce2aee4
Ben Morrison <bjm566@mun.ca>
2017-03-09 11:50:55 -0330

    use strict, foreach instead of for

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function generateMap(){
 	for(var y=0;y<tiles.length;y++){
 		tiles[y]=Array(game.map.x);
 		for(var x=0;x<tiles[y].length;x++){
-			tiles[y][x]={terrain:game.terrain[(Math.random()>0.8)?"grass":"dirt"],element:null};
+			tiles[y][x]={terrain:game.terrain[(Math.random()>0.8)?"grass":"dirt"]};
 		}
 	}
 	return tiles;
@@ -27,15 +27,13 @@ function generateMap(){
 
 //create html from hex grid
 function drawMap(){
-	var map=game.map.tiles;
 	var tileHeight=game.gfx.tileDim[1];
 	var tileWidth=game.gfx.tileDim[0];
 	var imageMap=document.createElement('map');
 	imageMap.name='backmap';
 	game.gfx.grid.appendChild(imageMap);
-	for(var y=0;y<map.length;y++){
-		for(var x=0;x<map[y].length;x++){
-			var tile=map[y][x];
+	game.map.tiles.forEach((row,y)=>{
+		row.forEach((tile,x)=>{
 			var img=document.createElement("img");
 			img.className = "point-through";
 			img.src=game.terrain.path(tile.terrain);
@@ -50,7 +48,6 @@ function drawMap(){
 			game.gfx.grid.appendChild(img);
 			var areaTag=document.createElement('area');
 			areaTag.shape="poly";
-			
 			var mid=add([left,top],[tileWidth/2,tileHeight/2]);
 			var shiftToMid=function(coord){
 				return add(mid,coord);
@@ -69,6 +66,7 @@ function drawMap(){
 			}
 			areaTag.onclick=Handler(x,y);
 			imageMap.appendChild(areaTag);
-		}
-	}
+		})
+	})
+	
 }

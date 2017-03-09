c8343a5a1e2de1cfc78050b4de68d9a00c11eed0
Ben Morrison <bjm566@mun.ca>
2017-03-09 11:22:00 -0330

    organised global variables, fixed side effects

--- main.js
+++ main.js
@@ -1,52 +1,44 @@
 "use strict";//do not delete, enables warnings
-//constants
-var tileWidth=64;
-var tileHeight=74;
-var x_squares = 2;
-var y_squares = 2;
-var map=[];
-var main;//the element containing the grid
-//array of offsets that are adjacent to the hex
-function get_adjacency(x,y){
-	//east clockwise to northeast, even/odd rows
-	var neighbours=[[[1,0],[0,1],[-1,1],[-1,0],[-1,-1],[0,-1]],
-				[[1,0],[1,1],[0,1],[-1,0],[0,-1],[1,-1]]];
-	return neighbours[y%2];
+var game;//persistent game state
+
+//initialise game data
+function init(){
+	game={
+		map:{x:10,y:10,tiles:null},
+		player:[],
+		terrain:{path:(name)=>{return "img/terrain/"+name+".png"},ext:".png",grass:"grass",dirt:"dirt"},
+		gfx:{tileDim:[64,74],grid:id("main")},
+		game:{}};
+	game.map.tiles=generateMap();
+	drawMap();
 }
+
+//create 2d hex grid
 function generateMap(){
-	var dim={x:x_squares,y:y_squares};
-	var seed=71;
-	for(var i=0;i<dim.x;i++){
-		map[i]=[];
-		for(var j=0;j<dim.y;j++){
-			var terrain="img/terrain/"+((Math.random()>0.8)?"grass":"dirt")	;
-			var tile=new MapTile(terrain,null);
-			map[i][j]=tile;
+	var tiles=Array(game.map.y);
+	for(var y=0;y<tiles.length;y++){
+		tiles[y]=Array(game.map.x);
+		for(var x=0;x<tiles[y].length;x++){
+			tiles[y][x]={terrain:game.terrain[(Math.random()>0.8)?"grass":"dirt"],element:null};
 		}
 	}
+	return tiles;
 }
-function MapTile(terrain,element){
-	this.terrain=terrain;
-	this.element=element;
-	//this.building="yes";
-}
-function init(){
-	console.log('initializing');
-	main=document.getElementById("main");
-	generateMap();
-	drawMap();
-	
-}
+
+//create html from hex grid
 function drawMap(){
+	var map=game.map.tiles;
+	var tileHeight=game.gfx.tileDim[1];
+	var tileWidth=game.gfx.tileDim[0];
 	var imageMap=document.createElement('map');
 	imageMap.name='backmap';
-	main.appendChild(imageMap);
+	game.gfx.grid.appendChild(imageMap);
 	for(var y=0;y<map.length;y++){
 		for(var x=0;x<map[y].length;x++){
 			var tile=map[y][x];
 			var img=document.createElement("img");
 			img.className = "point-through";
-			img.src=tile.terrain+".png";
+			img.src=game.terrain.path(tile.terrain);
 			var offset=(y%2==0)?0:tileWidth/2;//offset for odd rows
 			var topInterv=tileHeight - (tileWidth/2) / Math.sqrt(3);
 			var leftInterv=tileWidth;
@@ -55,8 +47,7 @@ function drawMap(){
 			img.style.position="absolute";
 			img.style.top=top+"px";
 			img.style.left=left+"px";
-			main.appendChild(img);
-			
+			game.gfx.grid.appendChild(img);
 			var areaTag=document.createElement('area');
 			areaTag.shape="poly";
 			
@@ -71,9 +62,12 @@ function drawMap(){
 				.map(shiftToMid)
 				.squish();
 			areaTag.coords=String(coords);
-			areaTag.onclick=function(event){
-				console.log(event);//make this do stuff
+			function Handler(x,y){
+				return function(event){
+					print([x,y]);
+				}
 			}
+			areaTag.onclick=Handler(x,y);
 			imageMap.appendChild(areaTag);
 		}
 	}
--- util.js
+++ util.js
@@ -1,24 +1,18 @@
+//everything is nondestructive unless noted
 
+//add two arrays
 function add(a,b){
-	var ret=Array(a.length);
-	for(var i=0;i<a.length;i++){
-		ret[i]=a[i]+b[i];
-	}
-	return ret;
+	return a.map((val,i)=>{return val+b[i]});
 }
 
+//add scalar to each element of array
 Array.prototype.addScalar=function(c){
-	for(var i=0;i<this.length;i++){
-		this[i]+=c;
-	}
-	return this;
+	return this.map((val)=>{return val+c});
 }
 
+//multiply each element by scalar
 Array.prototype.scale=function(c){
-	for(var i=0;i<this.length;i++){
-		this[i]*=c;
-	}
-	return this;
+	return this.map((val)=>{return val*c});
 }
 
 //one level of flattening
@@ -26,14 +20,37 @@ Array.prototype.squish=function(){
 	return this.concat.apply([],this);
 }
 
+//e^i*theta
 function cmplxExp(theta){
 	return [Math.cos(theta),-Math.sin(theta)];
 }
 
+//array containing 0..n
 function range(n){
 	return Array(n).fill(0).map(function(element,index){return index});
 }
 
+//pretty print
 function print(x){
 	console.log(JSON.stringify(x));
+}
+
+/**
+ * DOM Tools
+ */
+ 
+function id(id){
+	return document.getElementById(id);
+}
+
+/**
+ * Hexagon Tools
+ */
+
+//get offset of coordinates of adjacent tiles
+function get_adjacency(x,y){
+	//east clockwise to northeast, even/odd rows
+	var neighbours=[[[1,0],[0,1],[-1,1],[-1,0],[-1,-1],[0,-1]],
+				[[1,0],[1,1],[0,1],[-1,0],[0,-1],[1,-1]]];
+	return neighbours[y%2];
 }
